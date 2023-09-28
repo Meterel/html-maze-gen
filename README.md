54810bb543a4931903a6e9be207fd34d868432fb
Meterel <114000274+Meterel@users.noreply.github.com>
2023-09-28 22:58:23 +0200

    V3 update

--- README.md
+++ README.md
@@ -4,22 +4,24 @@
 
 Pros:
 
-* The algorithm and rendering is multithreaded using `Worker`s, dividing the maze in chunks and assigning them evenly to each thread
+* The generation is multithreaded using `Worker`s, dividing the maze in chunks and assigning them evenly to each thread
 
-* The maze is rendered with one `OffscreenCanvas` per chunk making the rendering multithreaded
+* The rendering is done with the WebGL context making it around 10 times faster than the old renderer that was using the 2D context
 
-* The algorithm stores occupied cells in a `Set` making the search time very little
+* When rendering a continuous series of walls, it'll be rendered as only one long wall, instead of rendering each wall individually in the series thus using 2 times less geometry
 
-* To use arrays in a `Set`, instead of converting them to a `String` with `JSON.stringify` and back with `JSON.parse`, I encode and decode them to a `Number` which is around 10 times faster, for example [86,64] -> 8600064
+* The algorithm keeps track of occupied cells in their matrix for fast access and in an `Array` used as a stack for fast iteration
 
-* Preallocation is used on `Array`s such as the ones where the maze's walls are stored
+* Preallocation is used on `Array`s such as the ones where the cells are stored
 
 #
 
 Cons:
 
-* Because the maze gets divided in chunks when multithreaded, the quality of the maze will drop if the gridsize is small
-
 * The maze might appear blurry on Chrome because it doesn't follow spec, making `image-rendering` only apply on `img` elements instead of [all elements](https://drafts.csswg.org/css-images/#propdef-image-rendering)
 
+* When multithreaded the maze gets divided in chunks making the quality of the maze drop if the gridsize is small
+
 * When multithreaded the borders between chunks aren't 100% seamless
+
+* Due to using WebGL for the new renderer and using only one `canvas` instead of multiple, there come limitations on the size of the maze
--- maze gen.js
+++ maze gen.js
@@ -1,52 +1,54 @@
-/*
-    Made by Meterel
-    https://github.com/Meterel/html-maze-gen
-*/
+// Made by Meterel
+// https://github.com/Meterel/html-maze-gen
 
 "use strict";
 
-onmessage=(e)=>{
-    const gridsize=e.data[0];
-    const few_dead_ends=e.data[1];
-    const context=e.data[2].getContext("2d",{alpha:false,desynchronized:true});
 
-    const walls=new Array(gridsize);
+onmessage=(e)=>{
+    const [
+        gridsize,
+        few_dead_ends,
+        px,
+        offset_x,
+        offset_y
+    ]=e.data;
+
+
+    const cells=new Array(gridsize);
     for(let x=0;x<gridsize;x++){
-        walls[x]=new Array(gridsize);
+        cells[x]=new Array(gridsize);
         for(let y=0;y<gridsize;y++){
-            walls[x][y]=[true,true];
+            cells[x][y]=[true,true,false]; //wall x, wall y, occupied
         }
     }
 
-    const occupied=new Set();
-    const search_idx=occupied.entries();
+
+    const occupied=[];
     let searching=false;
-    let pos=[Math.round(Math.random()*(gridsize-1)),Math.round(Math.random()*(gridsize-1))];
+    let pos=[Math.floor(Math.random()*gridsize),Math.floor(Math.random()*gridsize)];
     let rot;
 
-    while(occupied.size<gridsize**2){
+    for(;;){
         const posrot=[[1,0],[0,1],[-1,0],[0,-1]];
 
-        for(let i=3;i>=0;i--){
-            const r=Math.round(Math.random()*i);
+        for(let i=4;i>0;i--){
+            const r=Math.floor(Math.random()*i);
             const to=[pos[0]+posrot[r][0],pos[1]+posrot[r][1]];
-            if(to[0]>=0 && to[0]<gridsize && to[1]>=0 && to[1]<gridsize){
-                const toe=to[0]*100_000+to[1]; //encodes the array
-                if(!occupied.has(toe)){
-                    pos=to;
-                    rot=posrot[r];
-                    occupied.add(toe);
-                    searching=false;
-                    break;
-                }
+            if(to[0]>=0 && to[0]<gridsize && to[1]>=0 && to[1]<gridsize && !cells[to[0]][to[1]][2]){
+                pos=to;
+                rot=posrot[r];
+                occupied.push(to);
+                cells[to[0]][to[1]][2]=true;
+                searching=false;
+                break;
             }
             posrot.splice(r,1);
         }
 
         if(!posrot.length){
             if(!few_dead_ends || searching){
-                const n=search_idx.next().value[0];
-                pos=[Math.floor(n/100_000),n%100_000]; //decodes the array
+                pos=occupied.pop();
+                if(!pos) break;
                 continue;
             }
             const to=[pos[0]+rot[0],pos[1]+rot[1]];
@@ -54,16 +56,58 @@ onmessage=(e)=>{
             searching=true;
         }
 
-        walls[pos[0]-Math.min(rot[0],0)][pos[1]-Math.min(rot[1],0)][Math.abs(rot[0])]=false;
+        cells[pos[0]-Math.min(rot[0],0)][pos[1]-Math.min(rot[1],0)][Math.abs(rot[0])]=false;
     }
 
-    context.fillStyle="#fff";
+
+    const lines=[];
+    
+    for(let x=0;x<gridsize;x++) if(cells[x][0][1]) cells[x][0][0]=false;
+    for(let y=1;y<gridsize;y++) if(cells[0][y][0]) cells[0][y][1]=false;
+    
     for(let x=0;x<gridsize;x++){
+        const xp=offset_x+x*7*px;
+        let start=null;
+
         for(let y=0;y<gridsize;y++){
-            if(!(!y && walls[x][y][1]) && walls[x][y][0]) context.fillRect(x*7,y*7,8,1);
-            if(!(!x && walls[x][y][0]) && walls[x][y][1]) context.fillRect(x*7,y*7,1,8);
+            if(!start && cells[x][y][1]){
+                start=[xp,offset_y+y*7*px-px];
+            }else if(start && !cells[x][y][1]){
+                lines.push(...start,xp,offset_y+y*7*px);
+                start=null;
+            }
+        }
+
+        if(start && cells[x][gridsize-1][1]) lines.push(...start,xp,offset_y+gridsize*7*px);
+    }
+
+    for(let y=0;y<gridsize;y++){
+        const yp=offset_y+y*7*px;
+        let start=null;
+
+        for(let x=0;x<gridsize;x++){
+            if(!start && cells[x][y][0]){
+                start=[offset_x+x*7*px-px,yp];
+            }else if(start && !cells[x][y][0]){
+                lines.push(...start,offset_x+x*7*px,yp);
+                start=null;
+            }
+        }
+
+        if(start && cells[gridsize-1][y][0]) lines.push(...start,offset_x+gridsize*7*px,yp);
+    }
+    
+    /*
+    //the old geometry generator is kept here to compare benchmarks with the newer versions
+    for(let x=0;x<gridsize;x++){
+        const xp=offset_x+x*7*px;
+        for(let y=0;y<gridsize;y++){
+            const yp=offset_y+y*7*px;
+            if(cells[x][y][0]) lines.push(xp-px,yp,xp+7*px,yp);
+            if(cells[x][y][1]) lines.push(xp,yp-px,xp,yp+7*px);
         }
     }
+    */
 
-    postMessage(undefined);
+    postMessage(new Float32Array(lines));
 }
